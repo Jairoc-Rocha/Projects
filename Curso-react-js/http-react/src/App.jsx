74a887ea66faeaa0bdd0770fe54bee8f73fc6cde
Jairoc-Rocha <jairoc.rocha10@gmail.com>
2026-07-14 14:03:00 -0300

    hhh git commit -m hhhhh

---
+++ Curso-react-js/http-react/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Aula 01</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima
+        voluptatem nihil assumenda rerum iste recusandae aperiam placeat quas
+        laborum asperiores. Accusantium, veritatis. At assumenda non beatae nemo
+        praesentium, consectetur culpa.
+      </p>
+    </>
+  );
+}
+
+export default App;
