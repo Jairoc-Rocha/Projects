2f927b0139292f85cb7aeb7170ffed1b6298e5d2
Jairoc-Rocha <jairoc.rocha10@gmail.com>
2026-08-05 16:17:51 -0300

    uuuu

---
+++ Curso-react-js/react-investments/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500 p-2">react-investments</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, est
+        doloremque, voluptas delectus minima facere commodi quia reiciendis
+        quidem quos reprehenderit aspernatur esse, ipsam error natus? Iste
+        sapiente repellat ipsa.
+      </p>
+    </>
+  );
+}
+
+export default App;
