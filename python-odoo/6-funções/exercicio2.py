c77f5acd21bccff7ea0c5c83058e99853b6e7ae0
Jairoc-Rocha <jairoc.rocha10@gmail.com>
2026-09-03 16:49:37 -0300

    hhh

---
+++ python-odoo/6-funções/exercicio2.py
@@ -0,0 +1,17 @@
+# def maiorMenor(n):
+#     if n > 10:
+#         return print(f"O número é maior do que 10, resultado: {n}")
+#     else:
+#         return print(f"O número é menor do que 10, resulado: {n}")
+    
+# maiorMenor(15)
+# maiorMenor(8)
+
+def maiorMenor(n):
+    if n > 10:
+        return f"O número é maior do que 10, resultado: {n}"
+    else:
+        return f"O número é menor do que 10, resultado: {n}"
+
+print(maiorMenor(20))
+print(maiorMenor(5))
